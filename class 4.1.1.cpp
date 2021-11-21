db9ea04515b2e4a6587cec917805bc11145d9f80
Cradles <93244699+KCHcard@users.noreply.github.com>
2021-11-21 20:29:31 +0800

    Add files via upload

---
+++ class 4.1.1.cpp
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <string.h>
+
+int search(int arr[], int x,int sz)
+{
+	int left = 0;
+	int right = sz-1;
+	int k = 0;
+	while (left <= right)
+	{
+		k = (left + right) / 2;
+		if (x < arr[k])
+		{
+			right = k - 1;
+		}
+		else if (x > arr[k])
+		{
+			left = k + 1;
+		}
+		else
+			return k;
+		
+	}
+	return 0;
+}
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 7;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int ret = search(arr, k, sz);
+	if (ret == 0)
+		printf("找不到\n");
+	else 
+		printf("找到了%d\n", ret);
+	return 0;
+}
+
+//int year(int x)
+//{
+//	if ((x % 4 == 0 && x % 100 != 0) || (x % 400 == 0))
+//		return 1;
+//	else
+//		return 0;
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1000; i <= 2000; i++) 
+//	{
+//		if (year(i) == 1)
+//			printf("%d ", i);
+//
+//	}
+//	return 0;
+//}
+
+//int whether(int x)
+//{
+//	int y = 0;
+//	for (y = 2; y < x; y++)
+//	{
+//		if (x % y == 0)
+//			return 0;
+//	}
+//	return 1;
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	for(i=100; i<=200; i++)
+//	{
+//		if (whether(i) == 1)
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+////地址和数值
+////形参相当于实参的一份拷贝
+//void swap(int* px, int* py)
+//{
+//	int tmp = 0;
+//	tmp = *px;
+//	*px = *py;
+//	*py = tmp;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	swap(&a, &b);
+//	printf("%d %d", a, b);
+//	return 0;
+//}
+
+//int max(int x, int y)
+//{
+//	if (x > y)
+//	{
+//		return x;
+//	}
+//	else
+//		return y;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf_s("%d %d", &a, &b);
+//	c = max(a, b);
+//	printf("%d", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr[] = "hello world";
+//	memset(arr, '@', 3);
+//	printf("%s", arr);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[] = "cardles";
+//	char arr2[20] = "******************";
+//	strcpy_s(arr2, arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = Add(a, b);
+//	printf("%d", c);
+//	return 0;
+//}
