05e448c060f9481a1113a66884481ae9ec95e982
Cradles <93244699+KCHcard@users.noreply.github.com>
2021-11-24 11:28:12 +0800

    Add files via upload

--- class 4.1.1.cpp
+++ class 4.1.1.cpp
@@ -1,41 +1,95 @@
 #include <stdio.h>
 #include <string.h>
 
-int search(int arr[], int x,int sz)
-{
-	int left = 0;
-	int right = sz-1;
-	int k = 0;
-	while (left <= right)
-	{
-		k = (left + right) / 2;
-		if (x < arr[k])
-		{
-			right = k - 1;
-		}
-		else if (x > arr[k])
-		{
-			left = k + 1;
-		}
-		else
-			return k;
-		
-	}
-	return 0;
-}
+//函数声明：函数定义在，main函数后需声明
+//int Add(int x, int y);
+//函数的定义
+//int Add(int x, int y)/*从上到下读取*/
+//{
+//	int i = 0;
+//	i = x + y;
+//	return i;
+//}
+//#include "Add.h"
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf_s("%d %d", &a, &b);
+//	int sum = 0;
+//	sum = Add(a, b);
+//	printf("%d", sum);
+//	return 0;
+//}
 
-int main()
-{
-	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-	int k = 7;
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	int ret = search(arr, k, sz);
-	if (ret == 0)
-		printf("找不到\n");
-	else 
-		printf("找到了%d\n", ret);
-	return 0;
-}
+//int main()
+//{
+//	printf("%d", printf("%d", printf("%d", 43)));
+//	//43 2 1
+//	return 0;
+//}
+
+//int main()
+//{
+//	int len = 0;
+//	len = strlen("abc");
+//	//printf("%d\n", len);
+//	printf("%d\n", strlen("abc"));//链式访问
+//	return 0;
+//}
+
+//void Add(int* x)
+//{
+//	(*x)++;
+//}
+//
+//int main()
+//{
+//	int num = 0;
+//	Add(&num);
+//	printf("%d", num);
+//	Add(&num);
+//	printf("%d", num);
+//	Add(&num);
+//	printf("%d", num);
+//	return 0;
+//}
+
+//int search(int arr[], int x,int sz)
+//{
+//	int left = 0;
+//	int right = sz-1;
+//	int k = 0;
+//	while (left <= right)
+//	{
+//		k = (left + right) / 2;
+//		if (x < arr[k])	
+//		{
+//			right = k - 1;
+//		}
+//		else if (x > arr[k])
+//		{
+//			left = k + 1;
+//		}
+//		else
+//			return k;
+//		
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int ret = search(arr, k, sz);
+//	if (ret == 0)
+//		printf("找不到\n");
+//	else 
+//		printf("找到了%d\n", ret);
+//	return 0;
+//}
 
 //int year(int x)
 //{
